f6903554b463fa923fa41e2c8265837d2ed2b1d7
jos3moreno <jjmoreno0561@gmail.com>
2022-06-10 18:53:17 -0700

    Initial commit to github, only gets the data from  yelp API

---
+++ data.py
@@ -0,0 +1,27 @@
+types_of_food = []
+
+restaurants_data = []
+'''
+["Smokin J's BBQ", 4.5, '$$', ['14035 Midland Rd', 'Poway, CA 92064'], False]
+['Baba Kabob', 4.5, None, ['13538 Poway Rd', 'Ste C', 'Poway, CA 92064'], False]
+['Don Pollo', 4.5, '$$', ['13338 Poway Rd', 'Poway, CA 92064'], False]
+['In The Mix Yogurt', 4.5, '$', ['13450 Poway Rd', 'Poway, CA 92064'], False]
+['Wholly Crepe', 4.5, '$$', ['14045 Midland Rd', 'Poway, CA 92064'], False]
+["Niko's Halo-Halo & Sari-Sari", 4.0, '$$', ['14034 Poway Rd', 'Ste C', 'Poway, CA 92064'], False]
+['Sushi Lounge Poway', 4.0, '$$', ['12622 Poway Rd', 'Ste A', 'Poway, CA 92064'], False]
+['Taco Taco Poway', 4.0, '$', ['13429 Community Rd', 'Poway, CA 92064'], False]
+['Ichizen Sushi & Japanese Cuisine', 4.0, '$', ['13307 Poway Rd', 'Poway, CA 92064'], False]
+['El Ranchito Taco Shop', 4.0, '$', ['13654 Poway Rd', 'Ste 10', 'Poway, CA 92064'], False]
+['Greens Please Wellness Kitchen', 5.0, '$$', ['12202 Poway Rd', 'Ste 100', 'San Diego, CA 92064'], False]
+['The Kings Craft Coffee', 4.5, '$', ['14530 Espola Rd', 'Ste A', 'Poway, CA 92064'], False]
+['Tikka Lounge', 4.5, '$$', ['13513 Poway Rd', 'Poway, CA 92064'], False]
+['Mount Woodson Trail', 4.5, None, ['14644 Lake Poway Rd', 'Poway, CA 92074'], False]
+["Franco's Flapjack Family Restaurant", 4.0, '$$', ['14034 Poway Rd', 'Poway, CA 92064'], False]
+["O'Brien's Boulangerie", 4.5, '$$', ['13615 Stowe Dr', 'Poway, CA 92064'], False]
+['Mostra Coffee', 4.5, '$$', ['12045 Carmel Mountain Rd', 'Ste 302', 'San Diego, CA 92128'], False]
+['Tong Sake House', 4.5, '$$', ['12320 Poway Rd', 'Poway, CA 92064'], False]
+['The Boba Co', 4.0, '$', ['13526 Poway Rd', 'Ste A', 'Poway, CA 92064'], False]
+['Iron Pan Thai Kitchen', 4.0, '$$', ['13538 Poway Rd', 'Ste A', 'Poway, CA 92064'], False]
+'''
+
+
--- main.py
+++ main.py
@@ -1,16 +1,32 @@
-import requests 
+import requests
+from data import *
+import json
 
 YELP_END_POINT = "https://api.yelp.com/v3/businesses/search"
+BUSINESS_DETAILS = "https://api.yelp.com/v3/businesses/{id}"
 API_Key = "kc2UdKkJld9TTFN1aNDwn0jMxb8n8uHv__u_hDPXgih_PHowe9EAUp4Zo6QZYlySHncfOpN9iT8S95v18ldr2gYT7wULw10tMuLXZXp-TyIk-Jcuv4RfoNT_qyqhYnYx"
 HEADER = {'Authorization': 'bearer %s' % API_Key}
 parameters = {
     'location': 'Poway'
 }
 
-# Stock API
+
 response = requests.get(YELP_END_POINT, headers=HEADER, params=parameters)
 response.raise_for_status()
-data = response.json()['businesses'][0]['name']
+data = json.loads(response.text)
+businesses = data['businesses']
 
-# tun dictionary into a list
-print(data)
+
+# print(businesses[0]["categories"][0]['alias'])
+# Clean data from .json format into more readable form
+# NOTE: the API returns 20 restaurants
+for business in businesses:
+    types_of_food.append([business["categories"][0]['title']])
+    restaurants_data.append([business["name"], business["rating"], business.get("price"),
+                             business["location"]["display_address"], business["is_closed"]])
+
+
+# businesses_id: oxfQZ80ON1Dk1Lnq51EAjw  tYlETe8fXejtXTidRHlszg
+# print(len(restaurants_data))
+for i in range(len(restaurants_data)):
+    print(f'${restaurants_data[i]} \n ${types_of_food[i]}')
